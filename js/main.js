b9b1eea164cad2c023baa531890a7136d4517aab
Joel Gutiérrez <j333@live.com.ar>
2013-08-22 22:20:06 -0300

    Cambio de jszoom
    Elevate zoom a menos de 800 px en modo inner

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 	$('.carousel').carousel({
 	  	interval: 3000
 	});
-	$("#zoom").elevateZoom({
-            zoomType: "lens",
-            lensShape: "round",
-            lensSize: 250,
-            gallery: "gallery",
-            galleryActiveClass: "active",
+    $("#zoom").elevateZoom({
+        zoomType: "lens",
+        lensShape: "round",
+        lensSize: 250,
+        gallery: "gallery",
+        galleryActiveClass: "active",
     });
+    if ($(window).width() < 800) {
+        $("#zoom").elevateZoom({
+            zoomType: "inner",
+        });
+    }
 })
